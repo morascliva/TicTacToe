e78f20fedc568b55c726e5efe075133a56b56ad5
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2024-04-08 13:57:35 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Tictactoe from './components/TicTacToe/Tictactoe';
+
+
+
+function App() {
+  return (
+    <div >
+      <Tictactoe/>
+    </div>
+  );
+}
+
+export default App;
